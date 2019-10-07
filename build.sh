a368d7682e9f254dba0ecc00bb0fa7555c19ce82
xinau <felix.ehrenpfort@codecentric.de>
2019-10-07 22:45:34 +0200

    WIP: content/tour: generate tour from cue repository
    this commit adds the generation of the hugo files from the cue repository
    to the build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
+#!/bin/sh
+set -e
+set -x
+
 git submodule update -f --init --recursive
+
+PUSHD=$(pwd)
+cd vendor/github.com/cuelang/cue/doc/tutorial/basics
+go test .
+go run gen.go
+cd ${PUSHD}
+
+cp -rv vendor/github.com/cuelang/cue/doc/tutorial/basics/tour content/en/docs/
 
 # TODO
 #   shell documentation
