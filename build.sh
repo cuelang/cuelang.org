2b4e4a7da6f8cc89b23bbbb4e0097aba73465881
Marcel van Lohuizen <mpvl@golang.org>
2019-10-08 22:46:14 +0200

    content/docs/tour: generate from cue repo

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
+#!/bin/sh
+set -e
+set -x
+
 git submodule update -f --init --recursive
+
+PUSHD=$(pwd)
+cd content/en/docs/tutorials/tour
+go test .
+go run gen.go
+cd ${PUSHD}
 
 # TODO
 #   shell documentation
---
+++ content/en/docs/tutorials/tour/go.mod
@@ -0,0 +1,5 @@
+module cuelang.org/content/en/docs/tour
+
+go 1.12
+
+require github.com/rogpeppe/testscript v1.1.0
