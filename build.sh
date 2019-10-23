aa7179bb75049e6649789cac65f12d19e6a550e1
Marcel van Lohuizen <mpvl@golang.org>
2019-10-23 20:24:51 +0200

    Copy in language spec from cue repo (#41)
    * Copy in language spec from cue repo

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ set -x
 git submodule update -f --init --recursive
 
 PUSHD=$(pwd)
+cat <<EOF > content/en/docs/references/spec.md
++++
+title = "Language Specification"
++++
+EOF
+cat src/cue/doc/ref/spec.md >> content/en/docs/references/spec.md
 cd content/en/docs/tutorials/tour
 go test .
 go run gen.go
