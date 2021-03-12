79b29fb882186de0b111befeeefa03e77f756c25
Paul Jolly <paul@myitcv.io>
2021-03-12 15:53:28 +0000

    general: add a link to the CUE playground in the top menu
    Alongside the "Documentation", "Community" and "GitHub" links.
    
    In order that this also works for tip.cuelang.org, move to using
    configuration directories per:
    
    Per https://gohugo.io/getting-started/configuration/
    
    This allows us then to have a different Play link for tip.cuelang.org.
    
    Fixes #110

--- build.sh
+++ build.sh
@@ -10,11 +10,14 @@ then
 	exit 1
 fi
 
+env="production"
+
 # If we are running on netlify (i.e. a deploy) and we are building tip then we
 # need to grab the master of our cuelang.org/go and
 # github.com/cue-sh/playground dependencies
 if [ "$BRANCH" = "tip" ]
 then
+	env="tip"
 	GOPROXY=direct go get -d cuelang.org/go@master
 	# Now force cuelang.org/go  through the proxy so that the /pkg.go.dev redirect works
 	go get -d cuelang.org/go@$(go list -m -f={{.Version}} cuelang.org/go)
